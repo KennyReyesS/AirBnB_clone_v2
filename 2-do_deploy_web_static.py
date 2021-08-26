5a3108f896a0c7137490fdc0a81b90cde9e6b6d9
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-26 20:45:37 +0000

    script fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,28 +9,38 @@ from fabric.api import put, env, sudo
 import os
 
 
+env.user = 'ubuntu'
 env.hosts = ['34.139.28.87', '54.164.112.178']
 
 
 def do_deploy(archive_path):
-    if os.path.exists('archive_path') is False:
+    if os.path.isfile(archive_path) is False:
+        print("no se encontro")
         return False
     else:
         archive_split = archive_path.split('/')
         with_extension = archive_split[1]
         rm_extension = with_extension.split('.')
         name_archive = rm_extension[0]
-        put("archive_path, /tmp/{}".format(with_extension))
+        print("poniendo archivo")
+        put(archive_path, "/tmp/{}".format(with_extension))
+        print("creando carpeta con archivo")
         sudo("mkdir -p /data/web_static/releases/{}/".format(name_archive))
+        print("descomprimiendo")
         sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
             with_extension, name_archive))
+        print("eliminando archivo de tmp")
         sudo("rm /tmp/{}".format(with_extension))
+        print("moviendo archivo")
         sudo("mv /data/web_static/releases/{}/web_static/* "
              "/data/web_static/releases/{}/".format(
                  name_archive, name_archive))
+        print("eliminando archivo que movi")
         sudo("rm -rf /data/web_static/releases/{}/web_static".format(
             name_archive))
+        print("eliminando carpeta current")
         sudo("rm -rf /data/web_static/current")
+        print("enlace simbolico")
         sudo("ln -s /data/web_static/releases/{}/ "
              "/data/web_static/current".format(name_archive))
         return True
