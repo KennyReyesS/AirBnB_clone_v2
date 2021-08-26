790ce1cab55e38b0de24c5705ec575244067caa6
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-26 22:55:22 +0000

    try moved

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,11 +17,11 @@ def do_deploy(archive_path):
     """ Script that distributes an archive to my web servers"""
     if os.path.isfile(archive_path) is False:
         return False
-    try:
         archive_split = archive_path.split('/')
         with_extension = archive_split[1]
         rm_extension = with_extension.split('.')
         name_archive = rm_extension[0]
+    try:
         put(archive_path, "/tmp/{}".format(with_extension))
         sudo("mkdir -p /data/web_static/releases/{}/".format(name_archive))
         sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
@@ -36,5 +36,5 @@ def do_deploy(archive_path):
         sudo("ln -s /data/web_static/releases/{}/ "
              "/data/web_static/current".format(name_archive))
         return True
-    except BaseException:
+    except:
         return False
