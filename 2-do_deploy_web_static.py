5829ab9b52f785fdf5d9c677ac2465314036162e
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-26 20:59:04 +0000

    if else fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,9 +15,7 @@ env.hosts = ['34.139.28.87', '54.164.112.178']
 
 def do_deploy(archive_path):
     """ Script that distributes an archive to my web servers"""
-    if os.path.isfile(archive_path) is False:
-        return False
-    else:
+    if os.path.isfile(archive_path):
         archive_split = archive_path.split('/')
         with_extension = archive_split[1]
         rm_extension = with_extension.split('.')
@@ -36,3 +34,5 @@ def do_deploy(archive_path):
         sudo("ln -s /data/web_static/releases/{}/ "
              "/data/web_static/current".format(name_archive))
         return True
+    else:
+        return False
