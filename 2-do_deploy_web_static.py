c42bbffa08b0e5f44fd216fb04b3b82658a62ff6
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-26 20:53:41 +0000

    deleting prints

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,32 +16,23 @@ env.hosts = ['34.139.28.87', '54.164.112.178']
 def do_deploy(archive_path):
     """ Script that distributes an archive to my web servers"""
     if os.path.isfile(archive_path) is False:
-        print("no se encontro")
         return False
     else:
         archive_split = archive_path.split('/')
         with_extension = archive_split[1]
         rm_extension = with_extension.split('.')
         name_archive = rm_extension[0]
-        print("poniendo archivo")
         put(archive_path, "/tmp/{}".format(with_extension))
-        print("creando carpeta con archivo")
         sudo("mkdir -p /data/web_static/releases/{}/".format(name_archive))
-        print("descomprimiendo")
         sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
             with_extension, name_archive))
-        print("eliminando archivo de tmp")
         sudo("rm /tmp/{}".format(with_extension))
-        print("moviendo archivo")
         sudo("mv /data/web_static/releases/{}/web_static/* "
              "/data/web_static/releases/{}/".format(
                  name_archive, name_archive))
-        print("eliminando archivo que movi")
         sudo("rm -rf /data/web_static/releases/{}/web_static".format(
             name_archive))
-        print("eliminando carpeta current")
         sudo("rm -rf /data/web_static/current")
-        print("enlace simbolico")
         sudo("ln -s /data/web_static/releases/{}/ "
              "/data/web_static/current".format(name_archive))
         return True
