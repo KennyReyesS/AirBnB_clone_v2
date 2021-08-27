42f4293a807bbd7a7588e8d1b0cb1e09c261cafe
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-27 16:02:32 +0000

    unlink added

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,11 @@
 # Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
 sudo tee /data/web_static/releases/test/index.html <<EOF
 <html>
   <head>
@@ -16,9 +16,11 @@ sudo tee /data/web_static/releases/test/index.html <<EOF
   </body>
 </html>
 EOF
+sudo unlink /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 my_string="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 sed -i "/server_name _;/a $my_string" /etc/nginx/sites-available/default
+sudo service nginx reload
 sudo service nginx restart
 exit 0
