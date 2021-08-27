bb35fa14de225bf031a23abe045d573eace5a56b
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-27 21:42:48 +0000

    if failed added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,20 +22,25 @@ def do_deploy(archive_path):
     with_extension = archive_split[1]
     rm_extension = with_extension.split('.')
     name_archive = rm_extension[0]
-    try:
-        put(archive_path, "/tmp/{}".format(with_extension))
-        sudo("mkdir -p /data/web_static/releases/{}/".format(name_archive))
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            with_extension, name_archive))
-        sudo("rm /tmp/{}".format(with_extension))
-        sudo("mv /data/web_static/releases/{}/web_static/* "
-             "/data/web_static/releases/{}/".format(
-                 name_archive, name_archive))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(
-            name_archive))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{}/ "
-             "/data/web_static/current".format(name_archive))
-        return True
-    except:
+    if put(archive_path, "/tmp/{}".format(with_extension)).failed:
         return False
+    if sudo("mkdir -p /data/web_static/releases/{}/".format(name_archive)).failed:
+        return False
+    if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            with_extension, name_archive)).failed:
+        return False
+    if sudo("rm /tmp/{}".format(with_extension)).failed:
+        return False
+    if sudo("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(
+                name_archive, name_archive)).failed:
+        return False
+    if sudo("rm -rf /data/web_static/releases/{}/web_static".format(
+            name_archive)).failed:
+        return False
+    if sudo("rm -rf /data/web_static/current").failed:
+        return False
+    if sudo("ln -s /data/web_static/releases/{}/ "
+         "/data/web_static/current".format(name_archive)).failed:
+        return False
+    return True
