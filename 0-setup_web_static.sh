8bf6046e293d6dfb44c4fcc9bda4966d9989c9eb
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-27 16:14:24 +0000

    sudo added

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,20 +7,12 @@ sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
-sudo tee /data/web_static/releases/test/index.html <<EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | sudo tee /data/web_static/releases/test/index.html
 sudo unlink /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 my_string="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "/server_name _;/a $my_string" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a $my_string" /etc/nginx/sites-available/default
 sudo service nginx reload
 sudo service nginx restart
 exit 0
