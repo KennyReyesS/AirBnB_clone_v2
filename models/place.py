3bdbae87300f6d3b06c540e019c6a7654ce82429
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-12 00:57:18 +0000

    more imports added

--- models/place.py
+++ models/place.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from models.review import Review
 from models.amenity import Amenity
 from sqlalchemy.orm import relationship
 from os import getenv
 from sqlalchemy import *
+import models
 
 
 metadata = Base.metadata
